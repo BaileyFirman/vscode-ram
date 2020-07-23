22c791d6bf6774fb768749e775dff2f741a933b9
BaileyFirman <bailey.firman@nintex.com>
2020-07-23 21:08:55 +1200

    VSCRM-12: Converted tabs to spaces and moved execution and parsing out of activate function

--- src/extension.ts
+++ src/extension.ts
@@ -1,52 +1,51 @@
 import * as vscode from 'vscode';
 import { StatusBarItem, StatusBarAlignment } from 'vscode';
+
 const execSync = require('child_process').execSync;
 
 interface IStatusBarItemAlignment {
-	position: StatusBarAlignment;
-	priority: number;
+    position: StatusBarAlignment;
+    priority: number;
+};
+
+const executeCommand = (command: string): string => {
+    return execSync(command, { encoding: 'utf-8' });
+};
+
+const parseMemory = (result: string): number => {
+    const pressure = result.split('\n')[1].split(' ')[4];
+    return 100 - parseInt(pressure);
 };
 
 export const activate = () => {
 
-	const performanceButtonAllignment: IStatusBarItemAlignment = {
-		position: StatusBarAlignment.Left,
-		priority: 99999
-	};
+    const performanceButtonAllignment: IStatusBarItemAlignment = {
+        position: StatusBarAlignment.Left,
+        priority: 99998
+    };
 
-	const ramStatusText: string = '$(circuit-board)';
-	const ramStatus: StatusBarItem = 
-		buildStatusBarItem(performanceButtonAllignment, ramStatusText, '');
+    const ramStatusText: string = '$(circuit-board)';
+    const ramStatus: StatusBarItem =
+        buildStatusBarItem(performanceButtonAllignment, ramStatusText);
 
-	const executeCommand = (command: string) => {
-		return execSync(command, { encoding: 'utf-8' });
-	};
+    const memoryPressureCommand = 'memory_pressure -Q';
 
-	const memoryPressureCommand = 'memory_pressure -Q';
-	const cpuLoadCommand = `ps -e -o %cpu | awk '{s+=$1} END {print s}'`;
+    const pollMemory = () => {
+        setInterval(() => {
+            const memoryPressure = executeCommand(memoryPressureCommand);
+            const inversePercentage = parseMemory(memoryPressure);
+            ramStatus.text = `$(circuit-board) ${inversePercentage}%`;
+        }, 2000);
+    };
 
-		(() => {
-			setInterval(() => {
-				const memoryPressure = executeCommand(memoryPressureCommand);
-				const pressurePercentage = memoryPressure.split('\n')[1].split(' ')[4];
-				const inversePercentage = 100 - parseInt(pressurePercentage);
-				
-				const cpuLoad = executeCommand(cpuLoadCommand);
-				ramStatus.text = `$(circuit-board) ${inversePercentage}% | ${cpuLoad.slice(0, cpuLoad.length-1)}%`;
-			}, 2000);
-	})();
+    pollMemory();
 
-	ramStatus.show();
-}
+    ramStatus.show();
+};
 
-const buildStatusBarItem = (
-	alignment: IStatusBarItemAlignment,
-	iconName: string,
-	tooltip: string
-) => {
-	const button: StatusBarItem =
-		vscode.window.createStatusBarItem(alignment.position, alignment.priority);
-	button.text = iconName;
-	button.tooltip = tooltip;
-	return button;
+const buildStatusBarItem = (alignment: IStatusBarItemAlignment, iconName: string) => {
+    const button: StatusBarItem = vscode.window.createStatusBarItem(alignment.position, alignment.priority);
+    button.text = iconName;
+    button.tooltip = '';
+    return button;
 };
